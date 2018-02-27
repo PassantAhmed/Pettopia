aaf1dafd6c14f75a7909a766dfdb3179cf1f57a4
Passant <passantswellum@outlook.com>
2018-02-27 05:59:19 +0200

    validation changes

--- Pettopia/src/main/java/com/bfdaparent/pettopia/utilities/ValidationChecks.java
+++ Pettopia/src/main/java/com/bfdaparent/pettopia/utilities/ValidationChecks.java
@@ -8,8 +8,6 @@ public class ValidationChecks {
 
     private final Pattern EMAIL_PATTERN = Pattern.compile("^[_A-Za-z0-9-.]+([A-Za-z0-9-_.]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,})$");
     private final Pattern NAME_PATTERN = Pattern.compile("^[a-zA-Z[ ]{0,1}]{3,20}$");
-    private final Pattern USERNAME_PATTERN = Pattern.compile("^[a-zA-Z0-9_.]{3,20}$");
-    private final Pattern IP_PATTERN = Pattern.compile("^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]).([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]).([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]).([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$");
     private Matcher patternMatcher;
 
     /**
@@ -46,43 +44,6 @@ public class ValidationChecks {
     public boolean isName(String name) {
         patternMatcher = NAME_PATTERN.matcher(name);
         return !isEmptyString(name) && patternMatcher.matches();
-    }
-
-    /**
-    * check if the username is valid or not and also a unique one  
-    * @param username 
-    * @return - true if valid <br> 
-    *          - false if not 
-    **/
-    public boolean isUserName(String username) {
-        patternMatcher = USERNAME_PATTERN.matcher(username);
-        return !isEmptyString(username) && patternMatcher.matches();
-    }
-
-    /**
-    * check if the ip is valid or not 
-    * @param ip 
-    * @return - true if valid <br> 
-    *          - false if not 
-    **/
-    public boolean isIP(String ip) {
-        patternMatcher = IP_PATTERN.matcher(ip);
-        return !isEmptyString(ip) && patternMatcher.matches();
-    }
-
-    /**
-    * check if the age of the user is legal or not 
-    * @param dateOfBirth 
-    * @return - true if legal <br> 
-    *          - false if not 
-    **/
-    public boolean isLegalAged(LocalDate dateOfBirth){
-        boolean legalAge = false;
-        if(!isEmptyString(dateOfBirth.toString()) && dateOfBirth.getYear() <= 2000){
-            legalAge = true;
-        }
-    
-        return legalAge;
     }
     
     /**
