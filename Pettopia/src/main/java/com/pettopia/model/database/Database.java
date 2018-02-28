26db9f1fc2ba3f99edaefc9bfae4bd2aaaec7663
ahmed <ahmedsobhy.elgawesh@hotmail.com>
2018-02-28 08:37:48 +0200

    added interfaces of database and  implements for users and admin

---
+++ Pettopia/src/main/java/com/pettopia/model/database/Database.java
@@ -0,0 +1,38 @@
+package com.pettopia.model.database;
+import java.sql.*;
+
+public class Database {
+
+    private static Database instance;
+    private volatile Connection conn;
+
+    private Database() throws ClassNotFoundException, SQLException {
+
+
+        String dbUrl = "jdbc:mysql://.....ip of server.....:3306/....dbname....";
+        conn = DriverManager.getConnection(dbUrl , "....dbname..." , "..dbpassword....");
+
+    }
+
+
+    public static Database getInstance() throws SQLException, ClassNotFoundException {
+        if(instance == null)
+            instance = new Database();
+            
+        return instance;
+    }
+    
+    public  Connection getConnection()
+    {
+            return conn;
+    }
+
+    public void closeConnection() throws SQLException {
+        conn.close();
+    }
+
+  
+
+    
+ 
+}
