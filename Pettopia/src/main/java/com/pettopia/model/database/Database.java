e6e085ee6a7971f1506b1e08abbbcd3d4bf0393c
heshamkadry <hmohamedabdelkader@fcih1.com>
2018-03-02 14:14:23 +0200

    adding my port connection

--- Pettopia/src/main/java/com/pettopia/model/database/Database.java
+++ Pettopia/src/main/java/com/pettopia/model/database/Database.java
@@ -10,9 +10,16 @@ public class Database {
 
     private Database() throws ClassNotFoundException, SQLException {
 
-        Class.forName("oracle.jdbc.driver.OracleDriver");
+        try{
+                    Class.forName("oracle.jdbc.driver.OracleDriver");
         //step2 create  the connection object  
         conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "pet", "pet");
+        }catch (SQLException ex) {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        //step2 create  the connection object  
+        conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:xe", "pet", "pet");
+        }
+
     }
 
     public static Database getInstance() throws SQLException, ClassNotFoundException {
