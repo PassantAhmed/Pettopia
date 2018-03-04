4b63f99412b2f46e3038b228a620142cba74c7e5
heshamkadry <hmohamedabdelkader@fcih1.com>
2018-03-04 18:37:32 +0200

    48l kter

--- Pettopia/src/main/java/com/pettopia/model/database/Database.java
+++ Pettopia/src/main/java/com/pettopia/model/database/Database.java
@@ -10,15 +10,19 @@ public class Database {
 
     private Database() throws ClassNotFoundException, SQLException {
 
-        Class.forName("oracle.jdbc.driver.OracleDriver");
+              Class.forName("oracle.jdbc.driver.OracleDriver");
         //step2 create  the connection object  
-         try{
-             conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "pet", "pet");
-         }
-       catch(Exception e)
-       {
-           conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "pet", "pet");
-       }
+        try {
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "pet", "pet");
+        } catch (Exception e) {
+            try{
+                conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "pet", "pet");
+            }catch(Exception x)
+            {
+                conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:xe", "pet", "pet");
+
+            }
+        }
     }
 
     public static Database getInstance() throws SQLException, ClassNotFoundException {
---
+++ Pettopia/src/main/webapp/UserReviewScript.js
@@ -0,0 +1,42 @@
+function sendEmail() {  
+    var email = $("#uEmail").val();
+    var objMessage = { "email": email };
+    $.ajax({
+        url: 'UserReviewServlet',
+        type: 'GET',
+        contentType: 'application/json',
+        data: objMessage,
+        dataType: 'json',
+        success: function(value) {
+           // $("#test").val = value.firstName;
+            console.log("Success");
+           // console.log("value of name is:" + value.firstName);
+        }
+    });
+}
+
+
+function refresh() {
+    $.ajax({
+        url: 'UserReviewServlet',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        success: refreshUser
+    });
+}
+
+function refreshUser(val) {
+    var user = val;
+    console.log("job "+user.job);
+    if(user.firstName  !== "undefined")
+    {
+       $("#tableRows tr").remove();
+        console.log("user first name "+user.firstName);
+        $('#tableRows').append('<tr><td align="center">' + user.firstName + '</td><td align="center">' + user.lastName+ '</td ><td align="center">' + user.birthDate+ '</td > <td align="center">' + user.job+ '</td ><td align="center">' + user.email+ '</td > <td align="center">' + user.creditLimit.toString()+ '</td > <td align="center">' + user.creditNo + '</td></tr>'); 
+    }   
+    else{
+        console.log("undefined");
+    }
+    
+}
